6ae9e88347ebc7965e2004eee93ad64d74aa90a3
Carles Vargas <carlesvargas5@gmail.com>
2025-07-22 11:13:53 +0200

    style: update logo hover color and simplify release script
    - Change logo hover effect color from #646cffaa to #2931c0aa for better visual consistency
    - Remove git push from release script to prevent automatic pushes

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,7 @@ import HelloWorld from './components/HelloWorld.vue'
 }
 
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+  filter: drop-shadow(0 0 2em #2931c0aa);
 }
 
 .logo.vue:hover {
